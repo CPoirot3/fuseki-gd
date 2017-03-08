89e1a5572550b6db1e4b9bff7519b4c33863b781
hui.chen <hui.chen@hulu.com>
2017-03-08 14:14:02 +0800

    modify

--- webapp/js/gd/rules.js
+++ webapp/js/gd/rules.js
@@ -3,9 +3,21 @@
  */
 $("#confirm").click(function() {
   console.log("confirm");
+  console.log($("#scope").val());
+  $("#scope2").val($("#scope").val().split("#")[1]);
   $("#mainForm").removeClass("hidden");
 //    $("#targetButton").addClass("hidden");
 });
+
+$("#incidentSelect").click(function() {
+  $("#incidentParams").removeClass("hidden");
+});
+
+$("#paramsDefineButton").click(function(e) {
+  e.preventDefault();
+  $("#paramsDefineDiv").removeClass("hidden");
+  $("#paramsDefineButton").addClass("hidden");
+})
 
 var updateConcept = function() {
   $.post('http://localhost:15100/updateConcept', function(scopesResult) {
@@ -43,22 +55,28 @@ var init = function() {
 
 init();
 
-$("#concept").click(function() {
-  updateConcept();
-});
+// $("#concept").click(function() {
+//   updateConcept();
+// });
+//
+// $("#scope").click(function() {
+//   updateScope();
+// });
 
-$("#scope").click(function() {
-  updateScope();
-});
+// $("#scope2").click(function() {
+//   updateScope();
+// });
 
-var id = 1;
+
 var resultId = 1;
 $("#deduce").click(function() {
   var deduceParams = $('#formID').serialize();
-  deduceParams = deduceParams + "&id=" + id;
+  var mapParams = $('#modal-form').serialize();
+  deduceParams = deduceParams + "&" + mapParams+ "&id=" + resultId;
   console.log(deduceParams);
-  $.post('http://localhost:15100/deduce', deduceParams, function() {
 
+  $.post('http://localhost:15100/deduce', deduceParams, function() {
+    console.log("deduce send successful");
   });
 });
 
@@ -90,19 +108,7 @@ option = {
     }
   },
   yAxis: {
-//            type: 'value',
-////            boundaryGap: [0, '100%'],
-//            splitLine: {
-//                show: false
-//            }
   },
-//        series: [{
-//            name: '数据',
-//            type: 'line',
-//            showSymbol: false,
-//            hoverAnimation: false,
-//            data: buffer
-//        }]
   series: [
     {
       type: 'graph',
@@ -129,55 +135,70 @@ option = {
 
 var timer = null;
 
-$("#results").click(function(e) {
-  e.preventDefault();
-  buffer = [];
-  times = [];
+buffer = [];
+times = [];
+var params = {};
+var myChart = null;
 
-  // 基于准备好的dom，初始化echarts实例
-  var myChart = echarts.init(document.getElementById('main'));
 
-  // 使用刚指定的配置项和数据显示图表。
-  myChart.setOption(option);
-
-  var params = {};
-  params["id"] = resultId;
-
-  timer = setInterval(function() {
-    $.post('http://localhost:15100/data', params, function(result) {
-      if(result["value"] == null) {
-        clearTimeout(timer);
-      }
+var getResult = function() {
+  $.post('http://localhost:15100/data', params, function(result) {
+    if(result["value"] == null) {
+      // resultId++;
+      clearTimeout(timer);
+      return;
+    } else {
       buffer.push(result["value"]);
       times.push(processTime(result["time"]));
-    });
-
-    var links = buffer.map(function(item, i) {
-      return {
-        source: i,
-        target: i + 1
-      };
-    });
-    if(buffer.length > 7) {
-      buffer.shift();
-      times.shift();
-      links.pop();
     }
+  });
 
-    myChart.setOption({
-      xAxis: {
-        data: times
-      },
-      series: [{
-        links: links,
-        data: buffer
-      }]
-    });
-  }, 2000);
+  var links = buffer.map(function(item, i) {
+    return {
+      source: i,
+      target: i + 1
+    };
+  });
+  if(buffer.length > 7) {
+    buffer.shift();
+    times.shift();
+    links.pop();
+  }
 
-});
+  myChart.setOption({
+    xAxis: {
+      data: times
+    },
+    series: [{
+      links: links,
+      data: buffer
+    }]
+  });
+};
 
-$("#stop_results").click(function(e) {
+$("#results").click(function(e) {
   e.preventDefault();
-  clearInterval(timer);
+  myChart = echarts.init(document.getElementById('main'));
+  myChart.setOption(option);
+
+  params["id"] = resultId;
+  timer = setInterval(getResult, 1000);
 });
+
+var stop = true;
+$("#stop_results").click(function(e) {
+
+  e.preventDefault();
+  params["id"] = resultId;
+  if(stop) {
+    $("#stop_results").text("继续");
+    if (timer) {
+      clearInterval(timer);
+    }
+  } else {
+    $("#stop_results").text("停止展示");
+    timer = setInterval(getResult, 1000);
+  }
+  stop = !stop;
+});
+
